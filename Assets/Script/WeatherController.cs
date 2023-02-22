70d46b50c768270355637c0e606be6ebc95c529a
TeddyUm <43351089+TeddyUm@users.noreply.github.com>
2023-02-21 23:33:41 -0800

    Upload

---
+++ Assets/Script/WeatherController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// weather state
+public enum WEATHERSTATE
+{
+    SUNNY,
+    OVERCAST,
+    RAIN,
+    THUNDER
+}
+
+public class WeatherController : MonoBehaviour
+{
+    [SerializeField]
+    private WEATHERSTATE wState;
+    [SerializeField]
+    // weather change between max and min time (random)
+    private float weatherMaxTimer;
+    [SerializeField]
+    private float weatherMinTimer;
+
+    // for time check
+    private float weatherCurLimit;
+    private float weatherCurTimer = 0;
+    private bool isChangeWeather;
+
+    private float thunderTime = 0;
+    private float thunderMaxTime;
+    private bool isThunder;
+
+    void Start()
+    {
+        // starting is sunny weather
+        wState = WEATHERSTATE.SUNNY;
+        weatherCurLimit = Random.Range(weatherMinTimer, weatherMaxTimer);
+        SoundManager.Instance.Play("Sunny");
+
+        thunderMaxTime = Random.Range(1, 4);
+    }
+
+    void Update()
+    {
+        // change weather timer update
+        weatherCurTimer += Time.deltaTime;
+        if (isThunder)
+            thunderTime += Time.deltaTime;
+
+        if (weatherCurTimer > weatherCurLimit)
+        {
+            WeatherCheck();
+        }
+
+        if (isThunder && thunderTime > thunderMaxTime && wState == WEATHERSTATE.THUNDER)
+        {
+            ThunderOn();
+            Invoke("ThunderOff", 0.1f);
+        }
+    }
+
+    private void WeatherCheck()
+    {
+        // weather timer change using random
+        weatherCurLimit = Random.Range(weatherMinTimer, weatherMaxTimer);
+        weatherCurTimer = 0;
+        isChangeWeather = true;
+
+        // change weather depend on the weather state
+        if (wState == WEATHERSTATE.SUNNY && isChangeWeather)
+        {
+            SoundManager.Instance.Play("Overcast");
+            SoundManager.Instance.SoundFadeIn("Overcast");
+            SoundManager.Instance.SoundFadeOut("Sunny");
+            wState = WEATHERSTATE.OVERCAST;
+            ChangeWeather(WEATHERSTATE.OVERCAST);
+        }
+        else if (wState == WEATHERSTATE.OVERCAST && isChangeWeather)
+        {
+            SoundManager.Instance.Play("Rain");
+            SoundManager.Instance.SoundFadeIn("Rain");
+            SoundManager.Instance.SoundFadeOut("Overcast");
+            wState = WEATHERSTATE.RAIN;
+            ChangeWeather(WEATHERSTATE.RAIN);
+        }
+        else if (wState == WEATHERSTATE.RAIN && isChangeWeather)
+        {
+            SoundManager.Instance.Play("Thunder");
+            SoundManager.Instance.SoundFadeIn("Thunder");
+            SoundManager.Instance.SoundFadeOut("Rain");
+            wState = WEATHERSTATE.THUNDER;
+            ChangeWeather(WEATHERSTATE.THUNDER);
+            isThunder = true;
+        }
+        else if (wState == WEATHERSTATE.THUNDER && isChangeWeather)
+        {
+            SoundManager.Instance.Play("Sunny");
+            SoundManager.Instance.SoundFadeIn("Sunny");
+            SoundManager.Instance.SoundFadeOut("Thunder");
+            wState = WEATHERSTATE.SUNNY;
+            ChangeWeather(WEATHERSTATE.SUNNY);
+        }
+    }
+
+    // change weather paricle
+    private void ChangeWeather(WEATHERSTATE _wState)
+    {
+        if (_wState == WEATHERSTATE.SUNNY)
+        {
+            WeatherManager.Instance.OvercastOff();
+            WeatherManager.Instance.RainStop();
+            WeatherManager.Instance.ThunderStop();
+            isThunder = false;
+            isChangeWeather = false;
+        }
+        else if (_wState == WEATHERSTATE.OVERCAST)
+        {
+            WeatherManager.Instance.OvercastOn();
+
+            isChangeWeather = false;
+        }
+        else if (_wState == WEATHERSTATE.RAIN)
+        {
+            WeatherManager.Instance.RainPlay();
+
+            isChangeWeather = false;
+        }
+        else if (_wState == WEATHERSTATE.THUNDER)
+        {
+            WeatherManager.Instance.ThunderPlay();
+
+            isChangeWeather = false;
+        }
+    }
+
+    void ThunderOn()
+    {
+        WeatherManager.Instance.LightPower(3.0f);
+    }
+    void ThunderOff()
+    {
+        WeatherManager.Instance.LightPower(WeatherManager.Instance.darkLimit);
+        thunderTime = 0.0f;
+        thunderMaxTime = Random.Range(1, 4);
+    }
+}
---
+++ Assets/Script/WeatherManager.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour
+{
+    static public WeatherManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            Instance = this;
+        }
+    }
+
+    // weather particles
+    [SerializeField]
+    ParticleSystem rain;
+    [SerializeField]
+    ParticleSystem thunder;
+    // global light
+    [SerializeField]
+    private UnityEngine.Experimental.Rendering.Universal.Light2D sunLight;
+    // dark rate at overcast
+    public float darkLimit;
+
+    // for fade in out function
+    private bool isFadeInRain;
+    private float fadeInRainTimer;
+    private bool isFadeOutRain;
+    private float fadeOutRainTimer;
+    private bool isFadeInOverCast;
+    private float fadeInOvercastTimer;
+    private bool isFadeOutOvercast;
+    private float fadeOutOvercastTimer;
+
+    private void Start()
+    {
+        // init data
+        darkLimit = 0.5f;
+        fadeInRainTimer = 0.0f;
+        fadeOutRainTimer = 1.0f;
+        fadeInOvercastTimer = 1.0f;
+        fadeOutOvercastTimer = darkLimit;
+    }
+
+    private void Update()
+    {
+        RainCheck();
+        OvercastCheck();
+    }
+
+    private void RainCheck()
+    {
+        // fade in rain (using particle emit control)
+        if (isFadeInRain)
+        {
+            fadeInRainTimer += Time.deltaTime / 2;
+            RainDrop((int)(fadeInRainTimer));
+
+            if (fadeInRainTimer > 1.0f)
+            {
+                rain.Play();
+                fadeInRainTimer = 0.0f;
+                isFadeInRain = false;
+            }
+        }
+        // fade out rain particle
+        if (isFadeOutRain)
+        {
+            fadeOutRainTimer -= Time.deltaTime / 2;
+            RainDrop((int)(fadeOutRainTimer));
+            rain.Stop();
+
+            if (fadeOutRainTimer < 0.0f)
+            {
+                rain.Stop();
+                fadeOutRainTimer = 1.0f;
+                isFadeOutRain = false;
+            }
+        }
+    }
+
+    private void OvercastCheck()
+    {
+        // darker than before
+        if (isFadeInOverCast)
+        {
+            fadeInOvercastTimer -= Time.deltaTime / 2;
+            sunLight.intensity = fadeInOvercastTimer;
+            if (fadeInOvercastTimer < darkLimit)
+            {
+                sunLight.intensity = darkLimit;
+                fadeInOvercastTimer = 1.0f;
+                isFadeInOverCast = false;
+            }
+        }
+        // change to sunny weather
+        if (isFadeOutOvercast)
+        {
+            fadeOutOvercastTimer += Time.deltaTime / 2;
+            sunLight.intensity = fadeOutOvercastTimer;
+            if (fadeOutOvercastTimer > 1.0f)
+            {
+                sunLight.intensity = 1.0f;
+                fadeOutOvercastTimer = darkLimit;
+                isFadeOutOvercast = false;
+            }
+        }
+    }
+
+    // for set weather (bool change)
+    public void RainPlay()
+    {
+        isFadeInRain = true;
+    }
+
+    public void RainStop()
+    {
+        isFadeOutRain = true;
+    }
+
+    public void RainDrop(int num)
+    {
+        rain.Play();
+        rain.Emit(num);
+    }
+
+    public void ThunderPlay()
+    {
+        thunder.Play();
+    }
+
+    public void ThunderStop()
+    {
+        thunder.Stop();
+    }
+    public void OvercastOn()
+    {
+        isFadeInOverCast = true;
+    }
+
+    public void OvercastOff()
+    {
+        isFadeOutOvercast = true;
+    }
+    public void LightPower(float lightIntensity)
+    {
+        sunLight.intensity = lightIntensity;
+    }
+}
